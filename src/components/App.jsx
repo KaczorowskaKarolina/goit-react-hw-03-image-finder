e7ebe8b6d2ac54f72e9805becff940dbdce25dcb
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-10-30 18:25:08 +0100

    new app code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import Button from './Button';
